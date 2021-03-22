feb1ca36f918d0b5083dc0e0cc2fb84d1bb08e28
Joe Butcher <joe.butcher93@gmail.com>
2021-03-21 21:40:43 -0600

    going over todays work i came across simple syntax error

--- routes/api/v0.js
+++ routes/api/v0.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const gallery = require('../../data/gallery')
 
-const router = express.router()
+const router = express.Router()
 
 router.get('/api/v0/gallery', (req, res) => {
   res.json(gallery)
