dd286bee0490a4d205b9becf67983786880835d1
Joe Butcher <joe.butcher93@gmail.com>
2021-03-28 15:27:48 -0600

    added import structure

--- import.js
+++ import.js
@@ -1,1 +1,24 @@
-const mongoose = require('./_connection');
+require('dotenv').config();
+const mongoose = require('mongoose');
+// import seed
+const gallery = require('./seeds/gallery');
+const storage = require('./seeds/formStorage');
+
+// model
+const form = require('./models/form');
+const image = require('./models/image');
+const formCollection = require('./seeds/formStorage');
+
+mongoose.connect(process.env.MONGODB_URL, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+
+storage.insertMany(formCollection, function (error, form) {
+  if (error) {
+    console.log(error)
+  }
+  console.log('Data Import Complete.')
+  mongoose.connection.close()
+})
